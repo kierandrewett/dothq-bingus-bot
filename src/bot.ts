3d4d3deb79532c39455d6e9435c5d093baf97141
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-07-25 23:57:16 +0100

    ✨ Add handlers

--- src/bot.ts
+++ src/bot.ts
@@ -1,15 +1,41 @@
-import { AkairoClient } from "discord-akairo";
+import { 
+    AkairoClient, 
+    CommandHandler, 
+    InhibitorHandler, 
+    ListenerHandler 
+} from "discord-akairo";
+
+import { config } from "dotenv";
+
+config();
 
 class BingusBot extends AkairoClient {
+    public commandHandler: CommandHandler;
+    public inhibitorHandler: InhibitorHandler;
+    public listenerHandler: ListenerHandler
+
     constructor() {
         super({
-            // Options for Akairo go here.
+            ownerID: ["217562587938816000", "381541244360327168", "249448403803570176", "277822562116042753"]
         }, {
-            // Options for discord.js goes here.
+            disableMentions: 'everyone'
+        });
+
+        this.commandHandler = new CommandHandler(this, {
+            directory: './commands/',
+            prefix: '?'
+        });
+
+        this.inhibitorHandler = new InhibitorHandler(this, {
+            directory: './inhibitors/'
+        });
+
+        this.listenerHandler = new ListenerHandler(this, {
+            directory: './listeners/'
         });
     }
 }
 
 const client = new BingusBot();
 
-client.login('TOKEN');
+client.login(process.env.TOKEN);
