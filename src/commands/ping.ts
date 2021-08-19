782198802e379c2a7c8e5ddbd9db54d6b8fbc11d
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-08-19 21:56:48 +0100

    use super cool nerd ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,21 +1,36 @@
 import { Argument, Command } from "discord-akairo";
 import { Message, MessageEmbed } from "discord.js";
-import { inspect } from "util";
+import execa from "execa";
 
 export default class PingCommand extends Command {
     public constructor() {
         super('ping', {
             aliases: ['ping'],
+
+            args: [
+                {
+                    id: 'url',
+                    match: 'rest'
+                }
+            ]
         });
     }
 
-    public async exec(message: Message) {
-        const sent = await message.reply('Ping!');
-        const timeDiff = ((sent?.editedAt as any) || (sent?.createdAt as any)) - ((message.editedAt as any) || (message.createdAt as any));
-        
-        const embed = new MessageEmbed()
-            embed.setColor("#2f3136")
-            embed.setDescription(`🏓 **Ping** ${timeDiff}ms\n💓 **Heartbeat** ${Math.round(this.client.ws.ping)}ms`)
-        sent.edit(embed);
+    public async exec(message: Message, args: any) {
+        const process = execa("ping", [args.url ? args.url : "discord.com", "-c", "3"]);
+
+        let dmsg: Message;
+
+        process.stdout?.on("data", async (msg) => {
+            if(!dmsg) {
+                dmsg = await message.channel.send({
+                    content: `\`\`\`${msg.toString().trim()}\`\`\``
+                })
+            } else {
+                await dmsg.edit({
+                    content: `\`\`\`${dmsg.content.replace(/\`\`\`/g, "")}\n${msg.toString().trim()}\`\`\``
+                })
+            }
+        });
     }
 }
