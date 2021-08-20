5ea393cefef8df461b833d670aa3fcf13ccbdb87
Kieran <42723993+EnderDev@users.noreply.github.com>
2021-08-20 16:32:41 +0000

    search engine

--- src/commands/screenshot.ts
+++ src/commands/screenshot.ts
@@ -12,16 +12,51 @@ export default class ScreenshotCommand extends Command {
                     id: 'url',
                     type: 'string',
                     match: 'rest'
+                },
+                 {
+                    id: 'whole',
+                    match: 'flag',
+                    flag: '--whole'
+                },
+                 {
+                    id: 'engine',
+                    match: 'option',
+                    flag: 'engine:',
+                    default: 'google'
                 }
             ]
         });
     }
 
-    public async exec(message: Message, args: { url?: string }) {
+    public async exec(message: Message, args: { url?: string, whole?: boolean, engine?: string }) {
         if(args.url) {
           if(args.url && (args.url as any).startsWith("http")) args.url = args.url
           else {
-            args.url = `https://google.com/search?q=${args.url}`
+            let url = ""
+
+            switch(args.engine || "google") {
+              case "google":
+                url = "https://google.com/search?q=";
+                break;
+              case "ddg":
+              case "duckduckgo":
+                url = "https://duckduckgo.com/?q=";
+                break;
+              case "bing":
+                url = "https://bing.com/search?q=";
+                break;
+              case "ecosia":
+                url = "https://ecosia.com/search?q=";
+                break;
+              case "sp":
+              case "startpage":
+                url = "https://startpage.com/search?q=";
+                break;
+              default:
+                url = "https://google.com/search?q=";
+                break;
+            } 
+            args.url = `${url}${args.url}`
           }
         }
 
@@ -39,11 +74,15 @@ export default class ScreenshotCommand extends Command {
           const browser = await puppeteer.launch({ args: ['--no-sandbox', '--window-size=854,480'], defaultViewport: null });
           const page = await browser.newPage();
 
+          await page.setUserAgent(
+            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0"
+          );
+
           await page.goto(args.url || "");
 
          await page.setViewport({ width: 0, height: 0 });
 
-          const buff: any  = await page.screenshot();
+          const buff: any  = await page.screenshot({ fullPage: !!args.whole });
 
           const img = new MessageAttachment(buff, "output.png");
           message.channel.send(img)
