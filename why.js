d6b263ae678d5704fa58d791a6fdfe288b5da725
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-08-21 17:56:01 +0100

    why 2 !!!

--- why.js
+++ why.js
@@ -1,5 +1,7 @@
 const { spawn } = require("child_process")
 
-spawn("yarn", [""], { stdio: "inherit" }).on("close", () => {
-    spawn("yarn", ["dev"], { stdio: "inherit" });
+spawn("apt-get", ["install", "libnss"], { stdio: "inherit" }).on("close", () => {
+    spawn("yarn", [""], { stdio: "inherit" }).on("close", () => {
+        spawn("yarn", ["dev"], { stdio: "inherit" });
+    });
 });
